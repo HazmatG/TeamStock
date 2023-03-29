f4b418f5ebeccf664ea1806da727201d7e50b7f7
Azatbek <72803057+HazmatG@users.noreply.github.com>
2023-03-29 20:47:09 +0600

    Add files via upload

---
+++ lib/utils/cardwidgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateWidget extends StatefulWidget {
+  const CreateWidget({Key? key}) : super(key: key);
+
+  @override
+  _CreateWidgetState createState() => _CreateWidgetState();
+}
+
+class _CreateWidgetState extends State<CreateWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
